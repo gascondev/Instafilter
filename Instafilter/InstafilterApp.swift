4ab351bb063e37f70f9bf2330df50268a0d515e1
Álvaro Gascón <alvarogascon@hotmail.com>
2024-06-16 19:12:57 +0200

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Álvaro Gascón on 16/6/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
